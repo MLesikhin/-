68c1148fb0d47ad5e5005666626b8f5be5ba472a
Марсэль <marsnekto2@gmail.com>
2026-04-07 02:12:59 +0300

    Add files via upload

--- box.h
+++ box.h
@@ -1,11 +1,25 @@
+#ifndef BOX_H
+#define BOX_H
+
+#include <iostream>
+
 struct Box {
-    int _len;     // длина
-    int _width;   // ширина
-    int _height;  // высота
-    int _color;   // цвет
+    int len;
+    int width;
+    int height;
+    int volume;
+
+    // Конструктор по умолчанию: все стороны = 10
+    Box();
+
+    // Конструктор с одним параметром: все стороны = value
+    explicit Box(int value);
+
+    // Конструктор с тремя параметрами: len, width, height
+    Box(int l, int w, int h);
+
+    // Дружественная функция для перегрузки оператора вывода
+    friend std::ostream& operator<<(std::ostream& os, const Box& b);
 };
 
-void writeToFile(const char* fileName, const struct Box& box);
-void readFromFile(const char* fileName, struct Box& box);
-
-#endif // BOX_H 
+#endif // BOX_H
