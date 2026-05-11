a320e13b2132aa30cef2177b0dd88afc202d7d87
Марсэль <marsnekto2@gmail.com>
2026-05-11 21:16:37 +0300

    Add files via upload

---
+++ box.h
@@ -0,0 +1,25 @@
+#ifndef BOX_H
+#define BOX_H
+
+#include <iostream>
+
+struct Box {
+    int len;
+    int width;
+    int height;
+    int volume;
+
+    // Конструктор по умолчанию: все стороны = 10
+    Box();
+
+    // Конструктор с одним параметром: все стороны = value
+    explicit Box(int value);
+
+    // Конструктор с тремя параметрами: len, width, height
+    Box(int l, int w, int h);
+
+    // Дружественная функция для перегрузки оператора вывода
+    friend std::ostream& operator<<(std::ostream& os, const Box& b);
+};
+
+#endif // BOX_H
