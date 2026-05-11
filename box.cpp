bb914218d06b779343e1ccef27f698f24e466542
Марсэль <marsnekto2@gmail.com>
2026-05-11 22:23:27 +0300

    Update box.cpp

--- box.cpp
+++ box.cpp
@@ -1,19 +1,39 @@
 #include "box.h"
+#include <fstream>
 
-Box::Box() : len(10), width(10), height(10) {
-    volume = len * width * height;
-}
+// Конструктор по умолчанию (РК_1)
+Box::Box() : _len(10), _width(10), _height(10), _color(0), volume(1000) {}
 
-Box::Box(int value) : len(value), width(value), height(value) {
-    volume = len * width * height;
-}
+// Конструктор с одним параметром (РК_1)
+Box::Box(int val) : _len(val), _width(val), _height(val), _color(0), volume(val * val * val) {}
 
-Box::Box(int l, int w, int h) : len(l), width(w), height(h) {
-    volume = len * width * height;
-}
+// Конструктор с тремя параметрами: len, width, height (РК_1)
+Box::Box(int len, int width, int height) 
+    : _len(len), _width(width), _height(height), _color(0), volume(len * width * height) {}
 
-std::ostream& operator<<(std::ostream& os, const Box& b) {
-    os << "len = " << b.len << ", width = " << b.width
-       << ", height = " << b.height << ", volume = " << b.volume << '\n';
+// Переопределение оператора вывода << (РК_1)
+std::ostream& operator<<(std::ostream& os, const Box& box) {
+    os << "len = " << box._len 
+       << ", width = " << box._width 
+       << ", height = " << box._height 
+       << ", volume = " << box.volume;
     return os;
+}
+
+// Запись структуры в файл (ДЗ SW_4)
+void writeToFile(const char* fileName, const Box& box) {
+    std::ofstream outFile(fileName, std::ios::binary);
+    if (outFile.is_open()) {
+        outFile.write(reinterpret_cast<const char*>(&box), sizeof(Box));
+        outFile.close();
+    }
+}
+
+// Чтение структуры из файла (ДЗ SW_4)
+void readFromFile(const char* fileName, Box& box) {
+    std::ifstream inFile(fileName, std::ios::binary);
+    if (inFile.is_open()) {
+        inFile.read(reinterpret_cast<char*>(&box), sizeof(Box));
+        inFile.close();
+    }
 }
