50154530cfe8bb41befd4555bf3efcebb6681f27
Марсэль <marsnekto2@gmail.com>
2026-05-11 22:21:58 +0300

    Update box.h

--- box.h
+++ box.h
@@ -4,22 +4,29 @@
 #include <iostream>
 
 struct Box {
-    int len;
-    int width;
-    int height;
-    int volume;
+    int _len;    // длина
+    int _width;  // ширина
+    int _height; // высота
+    int _color;  // цвет
+    int volume;  // объём
 
-    // Конструктор по умолчанию: все стороны = 10
+    // Конструктор по умолчанию (РК_1)
     Box();
-
-    // Конструктор с одним параметром: все стороны = value
-    explicit Box(int value);
-
-    // Конструктор с тремя параметрами: len, width, height
-    Box(int l, int w, int h);
-
-    // Дружественная функция для перегрузки оператора вывода
-    friend std::ostream& operator<<(std::ostream& os, const Box& b);
+    
+    // Конструктор с одним параметром (РК_1)
+    Box(int val);
+    
+    // Конструктор с тремя параметрами: len, width, height (РК_1)
+    Box(int len, int width, int height);
 };
+
+// Переопределение оператора вывода << (РК_1)
+std::ostream& operator<<(std::ostream& os, const Box& box);
+
+// Запись структуры в файл (ДЗ SW_4)
+void writeToFile(const char* fileName, const Box& box);
+
+// Чтение структуры из файла (ДЗ SW_4)
+void readFromFile(const char* fileName, Box& box);
 
 #endif // BOX_H
