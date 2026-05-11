c03001dd36fab7f37969da20685c6d7ea49d8344
Марсэль <marsnekto2@gmail.com>
2026-05-11 22:23:52 +0300

    Update fraction.h

--- fraction.h
+++ fraction.h
@@ -4,26 +4,35 @@
 #include <iostream>
 
 struct Fraction {
-    int num;   // числитель
-    int den;   // знаменатель (всегда > 0)
+    int _numerator;   // числитель
+    int _denominator; // знаменатель
 
-    // Конструктор по умолчанию: 0/1
+    // Конструктор по умолчанию (ДЗ SW_4 + РК_1)
     Fraction();
-
-    // Конструктор с параметрами
+    
+    // Конструктор с параметрами (ДЗ SW_4 + РК_1)
     Fraction(int numerator, int denominator);
-
-    // Приведение дроби к несократимому виду
-    void normalize();
-
-    // Дружественная функция для вывода
-    friend std::ostream& operator<<(std::ostream& os, const Fraction& f);
 };
 
-// Арифметические операции
+// Вспомогательная функция: Алгоритм Евклида для нахождения НОД
+int gcd(int a, int b);
+
+// Сокращение дроби (ДЗ SW_4)
+int fracReduction(Fraction& frac);
+
+// Сложение дробей (РК_1)
 Fraction sum(const Fraction& fr1, const Fraction& fr2);
+
+// Вычитание дробей (РК_1)
 Fraction sub(const Fraction& fr1, const Fraction& fr2);
+
+// Умножение дробей (РК_1)
 Fraction mul(const Fraction& fr1, const Fraction& fr2);
+
+// Деление дробей (РК_1)
 Fraction div(const Fraction& fr1, const Fraction& fr2);
+
+// Переопределение оператора вывода << для Fraction
+std::ostream& operator<<(std::ostream& os, const Fraction& frac);
 
 #endif // FRACTION_H
