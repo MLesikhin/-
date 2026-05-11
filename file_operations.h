2d0f31809aa225090610ec5b54683dab6384db9c
Марсэль <marsnekto2@gmail.com>
2026-05-11 22:11:06 +0300

    Create file_operations.h

---
+++ file_operations.h
@@ -0,0 +1,7 @@
+#ifndef FILE_OPERATIONS_H
+#define FILE_OPERATIONS_H
+
+// Заполнение файла случайными числами
+void fillFile(const char* fileName, int min, int max, char delimeter, int cols, int countElements);
+
+#endif // FILE_OPERATIONS_H
