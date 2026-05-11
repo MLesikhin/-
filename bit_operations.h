40765353c2ef0a2cb6495fa87892b4a6fd417822
Марсэль <marsnekto2@gmail.com>
2026-05-11 22:07:58 +0300

    Update bit_operations.h

--- bit_operations.h
+++ bit_operations.h
@@ -1,6 +1,7 @@
 #ifndef BIT_OPERATIONS_H
 #define BIT_OPERATIONS_H
 
+// === ДЗ 2 ===
 // Задача 1: Перестановка полуслов (2 байта) в слове (4 байта)
 void swapHalfWord(unsigned int* words);
 
@@ -12,5 +13,13 @@ int getMaxBit(const int* array, int N);
 
 // Задача 4: Подсчёт числа единичных бит в числе
 unsigned char getCount1Bit(int number);
+
+// === ДЗ 3 ===
+// Задача 1: Циклический сдвиг всех чисел в массиве на N бит влево/вправо
+void cycleShiftArray(int* array, int size, int N, bool left);
+
+// Задача 2: Установка единичного бита и сброс бита (установка 0)
+void setBit(int* number, int numBit);
+void clearBit(int* number, int numBit);
 
 #endif // BIT_OPERATIONS_H
